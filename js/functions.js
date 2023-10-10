c1652821e6818da8716a45f13c120058c5c9bf0b
Alex <abodunov11@gmail.com>
2023-10-10 21:26:10 +0300

    задание 2.23

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+function checkLength (checkString, maxLength) {
+    return checkString.length <= maxLength; 
+};
+
+function polindrom (checkText) {
+    const newString = checkText.replaceAll(' ', '').toLowerCase();
+    const halfSymvol = Math.floor(checkText.length/2);
+    const lastChar = checkText.length - 1;
+for (let i =0; i < halfSymvol; i++) {
+if (newString.at(i) !== newString.at(- i -1)){
+    return false;
+};
+return true;
+};
+}; 
+
