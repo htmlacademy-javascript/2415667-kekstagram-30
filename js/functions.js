35d07a7838f2105b4dd1dd7d22d8cab4f17ef178
Alex <abodunov11@gmail.com>
2023-10-11 10:21:53 +0300

    еще пробелы

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 function checkLength (checkString, maxLength) {
-    return checkString.length <= maxLength; 
-};
+  return checkString.length <= maxLength;
+}
 // Cтрока короче 20 символов
 checkLength('проверяемая строка', 20); // true
 // Длина строки ровно 18 символов
@@ -9,21 +9,21 @@ checkLength('проверяемая строка', 18); // true
 checkLength('проверяемая строка', 10); // false
 
 function polindrom (checkText) {
-    const newString = checkText.replaceAll(' ', '').toLowerCase();
-    const halfSymvol = Math.floor(checkText.length / 2);
+  const newString = checkText.replaceAll(' ', '').toLowerCase();
+  const halfSymvol = Math.floor(checkText.length / 2);
 for (let i = 0; i < halfSymvol; i++) {
-if (newString.at(i) !== newString.at(- i -1)){
+  if (newString.at(i) !== newString.at(- i -1)){
     return false;
-};
-return true;
-};
-}; 
+  };
+  return true;
+  }
+}
 
 // Строка является палиндромом
 polindrom('топот'); // true
 // Несмотря на разный регистр, тоже палиндром
 polindrom('ДовОд'); // true
 // Это не палиндром
-polindrom('Кекс');  // false
+polindrom('Кекс'); // false
 
 polindrom('Лёша на полке клопа нашёл '); // true
