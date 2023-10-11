5442408db9d8b3b634a9dbfb57c30fe23ffcf106
Alex <abodunov11@gmail.com>
2023-10-11 10:28:37 +0300

    лишние точки с запятой

--- js/functions.js
+++ js/functions.js
@@ -12,9 +12,9 @@ function polindrom (checkText) {
   const newString = checkText.replaceAll(' ', '').toLowerCase();
   const halfSymvol = Math.floor(checkText.length / 2);
   for (let i = 0; i < halfSymvol; i++) {
-    if (newString.at(i) !== newString.at(- i -1)){
+    if (newString.at(i) !== newString.at(- i - 1)){
       return false;
-    };
+    }
     return true;
   }
 }
