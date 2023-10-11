e89d3796f0f080898e7e00715275947c68dd3df1
Alex <abodunov11@gmail.com>
2023-10-11 10:07:50 +0300

    добавил вызовы функций с примерами

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,12 @@
 function checkLength (checkString, maxLength) {
     return checkString.length <= maxLength; 
 };
-
+// Cтрока короче 20 символов
+checkLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkLength('проверяемая строка', 10); // false
 
 function polindrom (checkText) {
     const newString = checkText.replaceAll(' ', '').toLowerCase();
@@ -15,3 +20,11 @@ return true;
 };
 }; 
 
+// Строка является палиндромом
+polindrom('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+polindrom('ДовОд'); // true
+// Это не палиндром
+polindrom('Кекс');  // false
+
+polindrom('Лёша на полке клопа нашёл '); // true
